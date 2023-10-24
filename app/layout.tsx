b384c99f4a9d39d20006bca7ea3837c37f575472
Aditya Das <ziggydas@gmail.com>
2023-10-24 16:42:15 -0400

    selection color

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='px-10 sm:px-24 bg-secondary-0 body-font font-abcdiatype h-screen font-normal selection:bg-[#EFD98D] overflow-x-clip scrollbar-hide'>
+      <body className='px-10 sm:px-24 bg-secondary-0 body-font font-abcdiatype h-screen font-normal selection:bg-yellow-700 overflow-x-clip scrollbar-hide'>
         {children}
       </body>
     </html>
