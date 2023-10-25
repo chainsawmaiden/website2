8e5a544e294657c04299fdf6b66fad9ed423c8fb
Aditya Das <ziggydas@gmail.com>
2023-10-25 16:58:17 -0400

    BIG CHANGE TO HERO LAYOUT

--- components/hero.tsx
+++ components/hero.tsx
@@ -21,9 +21,9 @@ export default function Hero() {
 
     console.log('rendered')
 
-    function handleMouseOver({title, hoverImage, page}: {title: string, hoverImage: object, page: string}) {
+    function handleMouseOver({title, hoverImage, page, featured}: {title: string, hoverImage: object, page: string, featured: boolean}) {
         setIsHovering(true);
-        setHoverContent([title, hoverImage, page!=""]);
+        setHoverContent([title, hoverImage, page!="", featured]);
     }
 
     function handleMouseOut() {
@@ -69,7 +69,7 @@ export default function Hero() {
                 onMouseOut={handleMouseOutToggle}
                 >
                 <Image alt='v' src={boxView ? (hoverToggle ? iconListHover : iconList) : (hoverToggle ? iconGridHover : iconGrid)} className='hidden md:inline justify-self-center w-auto h-[1.05rem] transition mt-1 col-span-1'/>
-                <p className='text-primary-400 group-hover:text-[#a99d74] animate-pulse font-abcdiatype text-xs py-[0.15rem] align-middle'>[toggle]</p>
+                <p className='text-primary-400 group-hover:text-[#a99d74] animate-pulse font-abcdiatype text-xs py-[0.15rem] align-middle'>[toggle view]</p>
                 
             </div>
             
@@ -95,13 +95,22 @@ export default function Hero() {
                 <Filters />
                 <hr className={`solid border-primary-0`}></hr>
             </section>
-            <section className={`flex flex-col py-4 -mx-8 px-8 overflow-scroll scrollbar-hide ${!boxView && 'gap-y-3'}`}>
+            <section className={`flex flex-col pt-5 pb-4 -mx-8 px-8 overflow-scroll scrollbar-hide gap-y-3`}>
 
-                {categories.map((category, index) => (<div key={index} className='grid md:grid-cols-[0rem_auto] auto-rows-min'>
+                {[true, false].map((featured, featuredIndex) =>
+                <React.Fragment key={featuredIndex}>
+
+                {featured ?  
+                    <p className='-mb-2.5 font-times text-base text-primary-300 uppercase justify-self-center -mt-1'>✧ featured ✧</p>
+                        
+                    : <p className='-mb-2 font-times text-base text-primary-300 uppercase justify-self-center mt-1'><span className='font-abcfavorit text-basefavorit'>↓</span>&#160;&#160;archive&#160;&#160;<span className='font-abcfavorit text-basefavorit'>↓</span></p>}
+
+                {categories.map((category, categoryIndex) => (<div key={categoryIndex} className='grid md:grid-cols-[0rem_auto] auto-rows-min'>
                     
                     <Label>{category}</Label>
-
-                    <section key={index} className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 ${!boxView && 'md:grid-cols-none md:flex md:flex-wrap md:font-normal md:gap-x-4 md:justify-start'}`}>    
+                
+                    
+                    <section key={categoryIndex} className={`grid grid-cols-2 md:grid-cols-3 ${featured ? 'lg:grid-cols-3 xl:grid-cols-5' : 'lg:grid-cols-4 xl:grid-cols-6'} ${!boxView && 'md:grid-cols-none md:flex md:flex-wrap md:font-normal md:gap-x-4 md:justify-start'}`}>    
 
                         {projectsData.map((project, index) => {
 
@@ -118,14 +127,15 @@ export default function Hero() {
                             </>
 
                             const contentList = <>
-                                <p className=' group-hover:text-secondary-0 text-[.5rem] text-primary-300 font-normal font-abcdiatype mt-1 sm:mt-0.5 transition duration-300 w-4'>{`[${index+1 < 10 ? '0' : ''}${(index+1)}]`}</p>
+                                <p className={`group-hover:text-secondary-0 text-[.5rem] text-primary-300 font-normal font-abcdiatype transition duration-300 w-4 ${featured ? 'mt-0.5' : 'mt-1'}`}>{`[${index+1 < 10 ? '0' : ''}${(index+1)}]`}</p>
 
-                                <p className={` group-hover:text-secondary-0 text-primary-100 whitespace-nowrap text-5xl mr-3 transition duration-300 ${index % 2 == 0 ? 'capitalize font-abcfavorit text-5xlfavorit italic mt-1' : 'uppercase font-times'}`}>{project.title}</p>
+                                {featured ? <p className={` group-hover:text-secondary-0 text-primary-100 whitespace-nowrap mr-3 transition duration-300 group-odd:capitalize group-odd:font-abcfavorit group-odd:text-5xlfavorit group-odd:italic group-odd:-mt-2 group-even:text-5xl group-even:uppercase group-even:font-times`}>{project.title}</p>
+                                :  <p className={` group-hover:text-secondary-0 whitespace-nowrap text-3xl mr-3 transition duration-300 ${index % 2 == 0 ? 'capitalize font-abcfavorit text-[1.81rem] italic mt-0.5' : 'uppercase font-times'} ${featured ? 'text-primary-100' : 'text-primary-100'}`}>{project.title}</p>}
 
                                 <Image 
                                     src={project.displayImage}
                                     alt="project image"
-                                    className='sm:group-hover:invert-[100%] object-contain w-min mt-1 mr-1.5 mb-2 transition duration-300 max-w-[3.5rem]'
+                                    className={`sm:group-hover:invert-[100%] object-contain w-min mr-1.5 mb-2 transition duration-300 ${featured ? 'max-w-[3rem] mt-1' : 'max-w-[3rem] mt-1.5'}`}
                                 />
                             </>
 
@@ -141,9 +151,9 @@ export default function Hero() {
                             
                             return (
                                     
-                                project.category == category && (project.page != "" ? 
+                                ((project.featured && featured && project.category == category) || (!featured && project.category == category)) && (project.page != "" ? 
 
-                                    <Link className={`group flex flex-col transition ease-in hover:z-0 hover:scale-105 h-auto pb-2 break-inside-avoid w-full hover:bg-yellow-100 ${boxView ? 'rounded-sm' : 'md:flex-row hover:md:scale-[1.02] md:h-12 md:w-auto md:pb-0 md:-mr-2 hover:md:bg-yellow-400 hover:md:z-10'} ${myFilters.includes('all') || myFilters.some((r:string) => project.type.includes(r)) ? (boxView ? 'flex' : 'flex md:visible') : (boxView ? 'hidden' : 'hidden md:invisible md:flex')}`}
+                                    <Link className={`group flex flex-col transition ease-in hover:z-0 hover:scale-105 h-auto pb-2 break-inside-avoid w-full hover:bg-yellow-100 ${boxView ? 'rounded-sm' : `md:flex-row hover:md:scale-[1.02] md:pb-0 ${featured ? 'md:h-12 md:mb-0' : 'md:h-9 md:mb-0 opacity-95'} md:w-auto md:-mr-2 hover:md:bg-yellow-400 hover:md:z-10`} ${myFilters.includes('all') || myFilters.some((r:string) => project.type.includes(r)) ? (boxView ? 'flex' : 'flex md:visible') : (boxView ? 'hidden' : 'hidden md:invisible md:flex')}`}
                                         onMouseOver= {() => handleMouseOver(project)}
                                         onMouseOut={handleMouseOut}
                                         href={project.page}
@@ -155,7 +165,7 @@ export default function Hero() {
                                     
                                 : 
 
-                                    <div className={`group cursor-help flex flex-col transition ease-in hover:z-0 hover:scale-105 h-auto pb-2 break-inside-avoid w-full hover:bg-yellow-100 ${boxView ? 'rounded-sm' : 'md:flex-row hover:md:scale-[1.02] md:h-12 md:-mr-2 md:w-auto md:pb-0 hover:md:bg-yellow-400 hover:md:z-10'} ${myFilters.includes('all') || myFilters.some((r:string) => project.type.includes(r)) ? (boxView ? 'flex' : 'flex md:visible') : (boxView ? 'hidden' : 'hidden md:invisible md:flex')}`}
+                                    <div className={`group cursor-help flex flex-col transition ease-in hover:z-0 hover:scale-105 h-auto pb-2 break-inside-avoid w-full hover:bg-yellow-100 ${boxView ? 'rounded-sm' : `md:flex-row hover:md:scale-[1.02] md:pb-0 ${featured ? 'md:h-12 md:mb-0' : 'md:h-9 md:mb-0 opacity-95'} md:w-auto md:-mr-2 hover:md:bg-yellow-400 hover:md:z-10`} ${myFilters.includes('all') || myFilters.some((r:string) => project.type.includes(r)) ? (boxView ? 'flex' : 'flex md:visible') : (boxView ? 'hidden' : 'hidden md:invisible md:flex')}`}
                                     onMouseOver= {() => handleMouseOver(project)}
                                     onMouseOut={handleMouseOut}>
 
@@ -164,9 +174,11 @@ export default function Hero() {
                                     </div>)
                                 )
                             })
+                    
                         }
+                
                     </section>
-                </div>))}
+                </div>))}</React.Fragment>)}
             </section>
 
             <AnimatePresence>
@@ -178,7 +190,7 @@ export default function Hero() {
                         exit={{ opacity: 0}}
                         transition={{ duration: 0.2 }}
                         >
-                        <motion.div 
+                        {hoverContent[2] || hoverContent[3] ? <motion.div 
                             animate={{opacity: [1, .75]}}
                             transition={{ repeat: Infinity, repeatType: 'reverse', duration: 1.2}}>
                             
@@ -188,10 +200,16 @@ export default function Hero() {
                                 className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto max-w-none h-[15rem] sm:h-[30rem]'
                             />
 
-                            <p className={`px-1  fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase whitespace-nowrap z-20 ${hoverContent[2] ? 'text-base font-times text-white' : 'text-basefavorit font-abcfavorit text-primary-100 bg-secondary-0' }`}>
+                            <p className={`px-1 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase whitespace-nowrap z-20 ${hoverContent[2] ? 'text-base font-times text-white bg-primary-100' : 'text-basefavorit font-abcfavorit text-primary-100 bg-secondary-0' }`}>
                                 {hoverContent[2] ? 'click to open' : "PAGE COMING SOON"}
                             </p>
-                        </motion.div>
+                        </motion.div> : <Image
+                                src={hoverContent[1]}
+                                alt="project image" 
+                                className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-auto max-w-none h-[15rem] sm:h-[30rem]'
+                            />
+                        
+                        }
                     </motion.div>
                 }
             </AnimatePresence>
--- lib/data.ts
+++ lib/data.ts
@@ -12,7 +12,13 @@ import questionMark from "@/public/questionmark.png"
 import stemchests from "@/public/stemchests.png"
 import sculptor from "@/public/sculptor.png"
 import criterionCinemas from "@/public/criterion-cinemas.gif"
-import criterionCinemasHover from "@/public/criterion-hover.png"
+import blenderWorkhop from "@/public/blender-workshop.png"
+import dayHover from "@/public/day.png"
+import stockholmSyndrome from "@/public/stockholm-syndrome.png"
+import youngAndBeautiful from "@/public/young-and-beautiful.jpeg"
+import flowerBoy from "@/public/flower-boy.jpeg"
+import stemToTheSky from "@/public/stem-to-the-sky.gif"
+import stemToTheSkyDisplay from "@/public/stem-to-the-sky-display.png"
 
 export const links = [
     {
@@ -32,8 +38,8 @@ export const links = [
     }
   ] as const;
 
-export const filters = ["work", "school", "me", "design", "ux/tech"] as string[];
-export const categories = ["✮spotlight", "2023", "2022"] as string[];
+export const filters = ["design", "tech", "art", "work/freelance",] as string[];
+export const categories = ["2023", "2022"] as string[];
 
 export const projectsData = [
     {
@@ -41,102 +47,170 @@ export const projectsData = [
       displayImage: stemchests,
       hoverImage: stemchests,
       page: "",
-      type: ["design", "work", "ux/tech"] as string[],
+      type: ["design", "work/freelance", "tech"] as string[],
       description: ", designed products & branding",
-      category: "✮spotlight",
+      category: "2023",
+      featured: true,
     },
-    {
-      title: "All Of Me, All Of You",
-      displayImage: allOfMeAllOfYou,
-      hoverImage: allOfMeAllOfYou,
-      page: "",
-      type: ["design", "me"] as string[],
-      description: ", a multimedia installation",
-      category: "✮spotlight",
-    },
-    {
-      title: "Sculptor",
-      displayImage: sculptor,
-      hoverImage: sculptor,
-      page: "",
-      type: ["design", "me"] as string[],
-      description: ", a digital illustration.",
-      category: "✮spotlight",
-    },
-    {
-      title: "Council",
-      displayImage: council,
-      hoverImage: council,
-      page: "",
-      type: ["design", "me"] as string[],
-      description: ", an oil painting I created on a 24x20 canvas",
-      category: "✮spotlight",
-    },
-    {
-      title: "Criterion Cinemas",
-      displayImage: criterionCinemas,
-      hoverImage: criterionCinemas,
-      page: "",
-      type: ["design", "school"] as string[],
-      description: ", a rebranding project I made for my graphic design class",
-      category: "✮spotlight",
-    },
-
-
     {
       title: "Yale Law School",
       displayImage: questionMark,
       hoverImage: questionMark,
       page: "",
-      type: ["design", "ux/tech", "work"] as string[],
+      type: ["design", "tech", "work/freelance"] as string[],
       description: ", an internet exhibition (WIP)",
       category: "2023",
-    },
-    {
-      title: "Hobbes & Sushi",
-      displayImage: hobbesAndSushiHover,
-      hoverImage: hobbesAndSushiHover,
-      page: "",
-      type: ["design", "me"] as string[],
-      description: ", a shrine dedicated to my two cats",
-      category: "2023",
+      featured: false,
     },
     {
       title: "17o1 Records",
       displayImage: questionMark,
       hoverImage: seventeenohone,
       page: "",
-      type: ["design", "work"] as string[],
+      type: ["design", "work/freelance"] as string[],
       description: ", oversaw creative direction for signed artists (WIP)",
       category: "2023",
+      featured: false,
+    },
+    {
+      title: "Design at Yale",
+      displayImage: questionMark,
+      hoverImage: dayHover,
+      page: "",
+      type: ["design", "tech", "work/freelance"] as string[],
+      description: ", worked with undergraduate design & technology collective that worked with Yale organizations and startups.",
+      category: "2023",
+      featured: false,
+    },
+    {
+      title: "Hobbes & Sushi",
+      displayImage: hobbesAndSushiHover,
+      hoverImage: hobbesAndSushiHover,
+      page: "",
+      type: ["design"] as string[],
+      description: ", a poster shrine dedicated to my two cats",
+      category: "2023",
+      featured: true,
+    },
+    {
+      title: "Criterion Cinemas",
+      displayImage: criterionCinemas,
+      hoverImage: criterionCinemas,
+      page: "",
+      type: ["design"] as string[],
+      description: ", a rebranding project I made for my graphic design class",
+      category: "2023",
+      featured: true,
+    },
+    {
+      title: "Blender Workshop",
+      displayImage: blenderWorkhop,
+      hoverImage: blenderWorkhop,
+      page: "",
+      type: ["design", "work/freelance"] as string[],
+      description: ", instagram post I made to advertise a blender workshop",
+      category: "2023",
+      featured: false,
+    },
+    {
+      title: "Sculptor",
+      displayImage: sculptor,
+      hoverImage: sculptor,
+      page: "",
+      type: ["art"] as string[],
+      description: ", a digital illustration.",
+      category: "2023",
+      featured: true,
     },
     {
       title: "Blair Album Club",
       displayImage: bac,
       hoverImage: bacHover,
       page: "",
-      type: ["design", "school"] as string[],
+      type: ["design", "work/freelance"] as string[],
       description: " instagram posts I made for club promotion and outreach",
       category: "2023",
+      featured: false,
+    },
+    {
+      title: "Council",
+      displayImage: council,
+      hoverImage: council,
+      page: "",
+      type: ["art"] as string[],
+      description: ", oil on canvas, 24x20",
+      category: "2023",
+      featured: true,
+    },
+    {
+      title: "STEM to the Sky",
+      displayImage: stemToTheSkyDisplay,
+      hoverImage: stemToTheSky,
+      page: "",
+      type: ["design", "work/freelance"] as string[],
+      description: " weekly educational science instagram posts I designed, reached 6000+ users",
+      category: "2023",
+      featured: false,
     },
     {
       title: "Mushroom Forest",
       displayImage: mushroomForest,
       hoverImage: mushroomForest,
       page: "",
-      type: ["design", "me"] as string[],
-      description: ", a digital illustration I made in Procreate.",
+      type: ["art"] as string[],
+      description: ", a digital illustration I made in Procreate",
       category: "2023",
+      featured: false,
     },
 
-    
     {
-      title: "Bluetooth-Controlled Robotic Arm",
+      title: "All Of Me, All Of You",
+      displayImage: allOfMeAllOfYou,
+      hoverImage: allOfMeAllOfYou,
+      page: "",
+      type: ["art"] as string[],
+      description: ", a multimedia art installation",
+      category: "2022",
+      featured: true,
+    },
+    {
+      title: "Bluetooth Robotic Arm",
       displayImage: roboticArm,
       hoverImage: roboticArm,
-      page: "",
-      type: ["school", "ux/tech"] as string[],
-      description: ", a digital illustration I made in Procreate.",
+      page: "https://youtu.be/GPSEP8xgVMI",
+      type: ["tech"] as string[],
+      description: ", a glove-controlled robotic arm I designed and programmed with Arduino",
       category: "2022",
+      featured: true,
+    },
+    {
+      title: "Stockholm Syndrome",
+      displayImage: stockholmSyndrome,
+      hoverImage: stockholmSyndrome,
+      page: "",
+      type: ["art"] as string[],
+      description: ", acrylic on canvas, 30x30",
+      category: "2022",
+      featured: false,
+    },
+    {
+      title: "Young And Beautiful",
+      displayImage: youngAndBeautiful,
+      hoverImage: youngAndBeautiful,
+      page: "",
+      type: ["art"] as string[],
+      description: ", oil and magazine collage on canvas, 12x12",
+      category: "2022",
+      featured: false,
+    },
+    {
+      title: "Flower Boy",
+      displayImage: flowerBoy,
+      hoverImage: flowerBoy,
+      page: "",
+      type: ["art"] as string[],
+      description: ", oil on canvas, 24x20",
+      category: "2022",
+      featured: false,
     },
   ] as const;
