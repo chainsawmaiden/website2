350621f2d2066f461ec2404159e59b0e6018baf7
Aditya Das <ziggydas@gmail.com>
2023-11-05 21:21:54 -0500

    mobile fix

--- components/hero.tsx
+++ components/hero.tsx
@@ -194,13 +194,13 @@ export default function Hero() {
                             <motion.div 
                                 animate={hoverContent[2] || hoverContent[3] ? {opacity: [1, .75]} : {opacity: 1}}
                                 transition={{ repeat: Infinity, repeatType: 'reverse', duration: 1.2}}
-                                className='max-w-none w-auto h-[15rem] sm:h-[30rem] flex max-h-screen'
+                                className='max-w-none w-auto h-[15rem] sm:h-[30rem] flex'
                                 >
                                 
                                 <Image
                                     src={hoverContent[1]}
                                     alt="project image" 
-                                    className='w-fit max-h-full h-auto self-center'
+                                    className='w-auto max-h-full h-auto self-center'
                                 />
 
                                 {(hoverContent[2] || hoverContent[3]) && <p className={`px-1 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase whitespace-nowrap z-20 ${hoverContent[2] ? 'text-base font-times text-white bg-primary-100' : 'text-basefavorit font-abcfavorit text-primary-100 bg-secondary-0' }`}>
