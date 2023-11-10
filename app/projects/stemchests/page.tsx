6c6ea0605ec4a94c9e1dda38cca7239a1125226d
Aditya Das <ziggydas@gmail.com>
2023-11-10 18:15:58 -0500

    Update page.tsx

--- app/projects/stemchests/page.tsx
+++ app/projects/stemchests/page.tsx
@@ -93,7 +93,7 @@ export default function Page() {
       
 
       <hr className='border-solid '></hr>
-      <Link href='/' className='lf'>All Projects -></Link>
+      <Link href='/' className='lf'>All Projects →</Link>
       <hr className='border-solid '></hr>
       
     </section>
