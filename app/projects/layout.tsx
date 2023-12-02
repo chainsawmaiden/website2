467fcd44870cd50c5a084fb3dcfcd218a1723d1e
Aditya Das <ziggydas@gmail.com>
2023-12-01 20:41:24 -0500

    safari fix

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -8,7 +8,7 @@ export default function ProjectsLayout({
   }) {
     return (
         <SidebarLayout>
-            <section className='flex pf flex-col pr-2 justify-items-stretch gap-y-2 sm:gap-y-3 h-full w-full overflow-clip mb-20 mt-1 sm:mt-0'>
+            <section className='flex pf flex-col pr-2 justify-items-stretch gap-y-2 sm:gap-y-3 min-h-fith-full w-full mb-20 mt-1 sm:mt-0'>
                 {children}
             </section>
         </SidebarLayout>
--- components/sidebarLayout.tsx
+++ components/sidebarLayout.tsx
@@ -13,10 +13,10 @@ export default function SidebarLayout({children, exclude} : {children: React.Rea
     <main className="flex flex-col min-h-full h-auto">
         <Header />
         <section className='flex flex-col-reverse sm:grid grid-cols-[9.25rem_1fr] my-2 gap-x-6 '>
-          <article className='h-min sm:flex-col sm:flex hidden w-full mt-4 sm:mt-0 sticky self-start top-[3.25rem] sm:h-[calc(100vh-6rem)] min-h-full overflow-auto scrollbar-hide -ml-1 sm:opacity-25 transition duration-500 hover:opacity-100'>
+          <aside className='h-min sm:flex-col sm:flex hidden w-full mt-4 sm:mt-0 sticky self-start top-[3.25rem] sm:h-[calc(100vh-6rem)] min-h-full overflow-auto scrollbar-hide -ml-1 sm:opacity-25 transition duration-500 hover:opacity-100'>
               <ProjectBar exclude={exclude} />
             
-            </article>
+            </aside>
             {children}
             <div className='grow'></div>
             
