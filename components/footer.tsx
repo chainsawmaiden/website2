07643f706abf83d66313c6c5ef6088c1d2356421
Aditya Das <ziggydas@gmail.com>
2023-12-05 06:10:19 -0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,12 @@
 "use client"
 
 import React, {useState} from 'react'
-import {motion, AnimatePresence} from 'framer-motion';
 
 export default function Footer() {
 
-  const [expand, setExpand] = useState(false);
-
-  function handleMouseOver() {setExpand(true)}
-  function handleMouseOut() {setExpand(false)}
-
   return (
     <footer className='pb-4 pf text-primary-400 w-screen -ml-20'>
-      <p className='leading-5 px-20'>Aditya Das © 2023
+      <p className='leading-5 px-20'>Aditya Das © 2023 / React & Next.js
       </p>
     </footer>
   )
