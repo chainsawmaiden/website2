8a75db9614fd3b57a2d3284841b895ebbbf538c9
Aditya Das <ziggydas@gmail.com>
2023-12-13 20:58:28 -0500

    mobile layout columns

--- components/imageWithTextRight.tsx
+++ components/imageWithTextRight.tsx
@@ -5,7 +5,7 @@ import { StaticImageData } from 'next/image'
 
 export default function ImageWithTextRight({img, children}: {img: StaticImageData, children: React.ReactNode}) {
   return (
-    <div className={`grid grid-cols-1 lg:grid-cols-[2fr_1fr] flex-col justify-items-center sm:flex-row my-2 gap-6 rounded-md items-start scrollbar-hide h-min`}>
+    <div className={`lg:grid lg:grid-cols-[2fr_1fr] flex flex-col justify-items-center my-2 gap-x-6 rounded-md items-start scrollbar-hide h-min`}>
         <ImageModal src={img} rounded={true}/>
         <div className='flex flex-col gap-y-2 justify-self-start py-2'>{children}</div>
     </div>
